0f293315ec6f124b535bc1eb86cbedfb4cd51c12
James <jameschen625@gmail.com>
2021-05-10 16:31:41 -0400

    Created children method and initialize method

---
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -0,0 +1,36 @@
+require_relative 'tic_tac_toe'
+
+class TicTacToeNode
+
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    res = []
+    board.rows.each_with_index do |row, i1|
+      row.each_with_index do |col, i2|
+        if col == nil
+          new_mark = :o
+          new_mark = :x if next_mover_mark == :o
+          fake_board = board.dup
+          fake_board.rows[i1][i2] = new_mark
+          res << TicTacToeNode.new(fake_board, new_mark, [i1, i2])
+        end
+      end
+    end
+    res
+  end
+end
+
